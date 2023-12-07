b00b47b6f35a1fd2272d08aaa462427f54c75b29
Alex Bogushenko <bogushenkoas@ukr.net>
2023-12-07 19:19:17 +0300

    inner commit

--- main.cpp
+++ main.cpp
@@ -3,65 +3,79 @@
 
 using namespace std;
 
+
 class Toy {
 public:
-    Toy( string name) {
-        name_ = name;
-    }
-
-    string getName() {
-        return name_;
-    }
-    ~Toy() {
-        cout << "Toy " << name_ << " was dropped " << endl;
-    }
+   Toy(string name) {
+       name_ = name;
+   }
+   string getName() {
+       return name_;
+   }
+   ~Toy() {
+       cout << "Toy " << name_ << " was dropped" << std::endl;
+   }
 private:
-    string name_;
+   string name_;
 };
-
 
 class Dog {
 public:
+
     void getToy(shared_ptr<Toy> toy) {
-        if (toy == nullptr) {
+
+       if (toy == nullptr) {
             cout << "No toy to get." << endl;
-        } else if (toy_ == nullptr) {
-            toy_ = toy;
-            cout << "I got the toy: " << toy_->getName() << endl;
-        } else if (toy_ == toy) {
+       }
+
+       else if (toy_ == toy) {
             cout << "I already have this toy." << endl;
-        } else {
-            cout << "Another dog is playing with this toy." << endl;
+         }
+      
+           else if (toy.use_count() > 2) {
+            cout << "Another dog is playing with this toy." << std::endl;
         }
+         
+
+        else {
+            toy_ = toy;
+            cout << "I got a new toy: " << toy_->getName() << std::endl;
+        }
+        
+         
     }
 
+   
     void dropToy() {
-        if (toy_ == nullptr) {
-            cout << "Nothing to drop." << endl;
-        } else {
-            cout << "I dropped the toy: " << toy_->getName() << endl;
+        if (toy_) {
             toy_.reset();
+            cout << "I dropped the toy." << endl;
+        } else {
+            cout << "Nothing to drop." << endl;
         }
     }
-
+    
 private:
     shared_ptr<Toy> toy_;
 };
 
+
+
 int main() {
+  
     shared_ptr<Toy> toy1 = make_shared<Toy>("Ball");
     shared_ptr<Toy> toy2 = make_shared<Toy>("Bone");
-
+    
     Dog dog1;
     Dog dog2;
-
-    dog1.getToy(toy1);
-    dog2.getToy(toy2);
-
-    dog1.getToy(toy2);
-   
-    dog1.dropToy();
+    
+    dog1.getToy(toy1); 
+    dog1.getToy(toy1); 
+    dog2.getToy(toy1);
+    dog2.getToy(toy2); 
+    dog1.dropToy(); 
     dog2.dropToy();
+  
 
     return 0;
 }
