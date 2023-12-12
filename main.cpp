c9f57816476981610b1677ca24acf7a17d1bf482
Alex Bogushenko <bogushenkoas@ukr.net>
2023-12-12 11:40:13 +0300

    example

--- main.cpp
+++ main.cpp
@@ -3,79 +3,55 @@
 
 using namespace std;
 
+class Toy
+{
+  public:
+  string name;
 
-class Toy {
-public:
-   Toy(string name) {
-       name_ = name;
-   }
-   string getName() {
-       return name_;
-   }
-   ~Toy() {
-       cout << "Toy " << name_ << " was dropped" << std::endl;
-   }
-private:
-   string name_;
+  Toy(string name)
+  {
+    cout << "called constructor " << this << endl;
+    this->name = name;
+  }
+
+   Toy(const Toy &other)
+  {
+    cout << "called constructor copy " << this << endl;
+    this->name = other.name;
+  }
+
+  Toy& operator = (const Toy &other)
+  {
+    cout << "caled operator " << this << endl;
+    if(this != & other)
+    {
+       this->name = other.name;
+    }
+    return * this;
+  }
+
+  
+  ~Toy()
+  {
+    cout << "called destructor " << this << " " << name << endl;
+  }
 };
 
-class Dog {
-public:
-
-    void getToy(shared_ptr<Toy> toy) {
-
-       if (toy == nullptr) {
-            cout << "No toy to get." << endl;
-       }
-
-       else if (toy_ == toy) {
-            cout << "I already have this toy." << endl;
-         }
-      
-           else if (toy.use_count() > 2) {
-            cout << "Another dog is playing with this toy." << std::endl;
-        }
-         
-
-        else {
-            toy_ = toy;
-            cout << "I got a new toy: " << toy_->getName() << std::endl;
-        }
-        
-         
-    }
-
-   
-    void dropToy() {
-        if (toy_) {
-            toy_.reset();
-            cout << "I dropped the toy." << endl;
-        } else {
-            cout << "Nothing to drop." << endl;
-        }
-    }
-    
-private:
-    shared_ptr<Toy> toy_;
-};
-
-
+ 
 
 int main() {
-  
-    shared_ptr<Toy> toy1 = make_shared<Toy>("Ball");
-    shared_ptr<Toy> toy2 = make_shared<Toy>("Bone");
-    
-    Dog dog1;
-    Dog dog2;
-    
-    dog1.getToy(toy1); 
-    dog1.getToy(toy1); 
-    dog2.getToy(toy1);
-    dog2.getToy(toy2); 
-    dog1.dropToy(); 
-    dog2.dropToy();
-  
+
+  Toy toy1("Alex");
+  Toy toy2("Boris");
+
+  cout << toy1.name << endl;
+  cout << toy2.name << endl;
+
+  toy2 = toy1;
+
+  cout << toy1.name << endl;
+  cout << toy2.name << endl;
+
 
     return 0;
 }
